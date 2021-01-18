1874cb4f795c1160b94555e9613abec30216ce89
Berezo <77620429+Berezo@users.noreply.github.com>
2021-01-18 13:14:26 +0100

    Add files via upload

---
+++ projectgis/historyapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import WojnaTrzydziestoletnia
+
+# Register your models here.
+
+admin.site.register(WojnaTrzydziestoletnia)
---
+++ projectgis/historyapp/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    loginClass = forms.TextInput(attrs={'class': 'form-control'})
+    passwordClass = forms.PasswordInput(attrs={'class': 'form-control'})
+    login = forms.CharField(widget=loginClass, label='Login', max_length=50, required=True)
+    password = forms.CharField(widget=passwordClass, label='Password', max_length=50, required=True)
+
+class RegisterForm(forms.Form):
+    loginClass = forms.TextInput(attrs={'class': 'form-control'})
+    passwordClass = forms.PasswordInput(attrs={'class': 'form-control'})
+    login = forms.CharField(widget=loginClass, label='Login', max_length=50, required=True)
+    email = forms.CharField(widget=loginClass, label='Email', max_length=50, required=True)
+    password = forms.CharField(widget=passwordClass, label='Password', max_length=50, required=True)
+
